ac34e318298159633a36653e1ad8b1f07f4d6a8a
Yuting Tseng <michating5511@gmail.com>
2022-02-17 18:07:34 +0000

    added multiple datasets

--- basic/views.py
+++ basic/views.py
@@ -32,13 +32,36 @@ class HomeView(generic.TemplateView):
 class SearchView(generic.ListView):
     template_name = 'basic/search.html'
 
+            
     def get_queryset(self): #overwrite the get_queryset method
+        global query
         query = self.request.GET.get('q', None)
+        global variant
+        variant = self.request.GET.get('v', None)
 
-        if query is not None:
-            basic_results = Basicinfo2.objects.raw(
-                'SELECT * FROM BasicInfo2 LEFT JOIN SigPepFin ON uniprot_id=sp_uniprot_id WHERE uniprot_id = %s', [query])
+        if query and variant is not None:
 
-            return basic_results
-        return Basicinfo2.objects.none()
+            
+            global topo_results
+            topo_results = Topodom.objects.raw(
+                'SELECT id, topology FROM (SELECT * FROM TopoDom WHERE uniprot = %s) AS tab WHERE topo_start <= %s and topo_end >= %s', [query, variant, variant])
+            global variant_results
+            variant_results = MissenseVarCom.objects.raw(
+                'SELECT * FROM Missense_Var_Com WHERE uniprot = %s and posuniprot = %s', [query, variant])
+            
+            final_results= list(chain(topo_results, variant_results))
+
+
+            return  final_results
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["topo_results"] = topo_results
+        context["variant_results"] =variant_results
+        return context
+
+        return  Topodom.objects.none()
+
+
+
 
